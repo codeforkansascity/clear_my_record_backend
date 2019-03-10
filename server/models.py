6b98a4b499f550ab63c316400fa991bfa2563179
Gabby Ortman <gortman@rednovalabs.com>
2019-03-10 12:11:15 -0500

    endpoints and migrations shell

--- server/models.py
+++ server/models.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 from clear_my_record_backend.server import dbs
 
-
 class Qualifying_Questions(dbs.Model):
     id = dbs.Column(dbs.Integer, primary_key=True)
     question = dbs.Column(dbs.Text)
-    help_text = dbs.Column(dbs.String(250))
+    help_text = dbs.Column(dbs.Text)
     disqualifying_answer = dbs.Column(dbs.String(250))
 
 
@@ -16,9 +15,8 @@ class Qualifying_Answers(dbs.Model):
     question_id = dbs.Column(dbs.String(250))
     answer = dbs.Column(dbs.Text)
     qualifying_answer = dbs.Column(dbs.String(250))
-    question_version_number = dbs.Column(dbs.Decimal)
+    question_version_number = dbs.Column(dbs.Float(asdecimal=True))
     timestamp = dbs.Column(dbs.DateTime, index=True, default=datetime.utcnow)
 
-
-class Users(dbs.Model):
-    pass
+# class Users(dbs.Model):
+#     pass
--- server/routes.py
+++ server/routes.py
@@ -5,3 +5,15 @@ from clear_my_record_backend.server import cmr
 @cmr.route('/index')
 def index():
     return "Hello, World!"
+
+@cmr.route('/qualifying_question', methods=['GET'])
+def qualifying_question():
+    pass
+
+@cmr.route('/qualifying_questions', methods=['GET'])
+def qualifying_questions():
+    pass
+
+@cmr.route('/qualifying_answer', methods=['POST'])
+def qualifying_answer():
+    pass
