5cf48c48645e964f4ec676d7b634c5cd5406396e
Gabby Ortman <gortman@rednovalabs.com>
2019-03-11 13:44:15 -0500

    flask db revision -m will make a new revision/data migration

---
+++ migrations/versions/6eea6be065fb_qualifying_questions_data_migration.py
@@ -0,0 +1,24 @@
+"""qualifying questions data migration
+
+Revision ID: 6eea6be065fb
+Revises: 9f02092798e8
+Create Date: 2019-03-11 11:54:28.063618
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6eea6be065fb'
+down_revision = '9f02092798e8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
