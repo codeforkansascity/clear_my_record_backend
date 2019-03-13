4da123b5b5f8394c3e1f1be2d6374a1e38f438e7
elias <elias.jm.garcia@gmail.com>
2019-03-13 17:17:54 -0500

    setup shell context for dev

--- cmr_app.py
+++ cmr_app.py
@@ -1,1 +1,7 @@
-from clear_my_record_backend.server import cmr
+from clear_my_record_backend.server import cmr, dbs
+from clear_my_record_backend.server.models import Users
+
+
+@cmr.shell_context_processor
+def make_shell_ctx():
+    return {'dbs': dbs, 'User': Users}
