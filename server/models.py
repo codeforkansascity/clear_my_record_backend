29c628cf57d5586cc8103e58f63988a68f7628dd
elias <elias.jm.garcia@gmail.com>
2019-03-13 20:27:42 -0500

    added tables for submission, changed naming to singular, added foreign keys and relationships

--- server/models.py
+++ server/models.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from clear_my_record_backend.server import dbs
 
-class Qualifying_Questions(dbs.Model):
+
+class Qualifying_Question(dbs.Model):
     id = dbs.Column(dbs.Integer, primary_key=True)
     question = dbs.Column(dbs.Text)
     help_text = dbs.Column(dbs.Text)
     disqualifying_answer = dbs.Column(dbs.String(250))
 
-class Qualifying_Answers(dbs.Model):
+
+class Qualifying_Answer(dbs.Model):
     # First Integer ID is set as autoincrement in SQLAlchemy
     id = dbs.Column(dbs.Integer, primary_key=True)
     user_session = dbs.Column(dbs.String(250), index=True, unique=True)
@@ -24,5 +26,20 @@ class Qualifying_Answers(dbs.Model):
         for key in kwargs:
             setattr(self, key, kwargs[key])
 
-# class Users(dbs.Model):
-#     pass
+
+class User(dbs.Model):
+    id = dbs.Column(dbs.Integer, primary_key=True)
+    username = dbs.Column(dbs.String(32), index=True, unique=True)
+    email = dbs.Column(dbs.String(120), index=True, unique=True)
+    pw_hash = dbs.Column(dbs.String(128))
+    submissions = dbs.relationship(
+        "Submission", backef="author", lazy="dynamic")
+
+    def __repr__(self):
+        return "<USER: {}".format(self.username)
+
+
+class Submission(dbs.Model):
+    id = dbs.Column(dbs.Integer, primary_key=True)
+    submitter_id = dbs.Column(dbs.Integer, dbs.ForeignKey("users.id"))
+    session_sub = dbs.Column(dbs.Integer, dbs.Foreign("qualifying_answers.id"))
