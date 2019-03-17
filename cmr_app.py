55f67e83d628898151a6c365530173f4bd621318
elias <elias.jm.garcia@gmail.com>
2019-03-17 13:56:59 -0500

    refactors cmr_app to actually work and also include other tables of interest

--- cmr_app.py
+++ cmr_app.py
@@ -1,7 +1,13 @@
 from clear_my_record_backend.server import cmr, dbs
-from clear_my_record_backend.server.models import Users
+from clear_my_record_backend.server.models import (User, Submission,
+                                                   Qualifying_Answer)
 
 
 @cmr.shell_context_processor
 def make_shell_ctx():
-    return {'dbs': dbs, 'User': Users}
+    return {
+        'dbs': dbs,
+        'User': User,
+        'Submission': Submission,
+        'Qualifying_Answer': Qualifying_Answer
+    }
