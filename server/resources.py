359d34808feed30aa645c55632ce5b7e192dbbd1
elias <elias.jm.garcia@gmail.com>
2019-04-07 16:49:38 -0500

    basic usage of jwt works

--- server/resources.py
+++ server/resources.py
@@ -1,9 +1,10 @@
-from flask import request, abort, Response
+from flask import request, abort, Response, jsonify
 from sqlalchemy.exc import SQLAlchemyError
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import (jwt_required, create_access_token,
-                                get_jwt_identity)
+                                create_refresh_token, get_jwt_identity)
 from clear_my_record_backend.server.models import User, save_to_dbs
+
 
 class Register(Resource):
     def post(self):
@@ -19,20 +20,31 @@ class Register(Resource):
         if new_user.exists():
             # TODO: some better exception handlers
             # This clumps both existing emails and usernames, separate erros.
-            abort(400)
+            msg = "Failure: User already exists."
+            return Response(msg, status=400, mimetype='text/plain')
+            # abort(400)
 
         try:
             save_to_dbs(new_user)
             msg = "Success. User {} created".format(new_user.username)
-            return Response(msg, status=200, mimetype='text/plain')
+            # return Response(msg, status=200, mimetype='text/plain')
         except SQLAlchemyError as e:
-
             msg = "Error while updating database: {}".format(e)
             return Response(msg, status=500)
         except Exception as e:
             # TODO: Try to get something more specific here for debuggin, etc.
             msg = "Unanticipated Error: {}".format(e)
             return Response(msg, status=500)
+
+        token = create_access_token(identity=new_user.username)
+        refresh = create_refresh_token(identity=new_user.username)
+        data = {'status': "success", 'data': {
+            'type': "bearer",
+            'token': token,
+            'refreshToken': refresh}}
+        # causes error lol
+        return data
+
 
 class Login(Resource):
     def post(self):
