2feef416b447d2e1c5c14872889abe4dc516b63e
Gabby Ortman <gortman@rednovalabs.com>
2019-04-08 11:10:00 -0500

    two way nesting

--- server/schemas.py
+++ server/schemas.py
@@ -1,7 +1,23 @@
 from clear_my_record_backend.server import ma
 from clear_my_record_backend.server.models import *
 
+
+class ChargesSchema(ma.ModelSchema):
+    class Meta:
+        model = Charge
+        fields = ()
+
+class ConvictionSchema(ma.ModelSchema):
+    charge = ma.Nested(ChargesSchema, many=True)
+
+    class Meta:
+        model = Conviction
+        fields = ()
+
 class ClientSchema(ma.Schema):
+    convictions = ma.Nested(ConvictionSchema, many=True)
+    user = ma.Nested('UserSchema', exclude=('clients', ))
+
     class Meta:
         model = Client
         fields = (
@@ -22,6 +38,7 @@ class ClientSchema(ma.Schema):
             'license_expiration_date',
             'status',
             'active',
+            'user'
         )
 
 class UserSchema(ma.ModelSchema):
@@ -30,9 +47,3 @@ class UserSchema(ma.ModelSchema):
     class Meta:
         model = User
         fields = ('id', 'username', 'clients')
-
-
-# class ConvictionSchema(ma.ModelSchema):
-#     class Meta:
-#         model = Conviction
-#         fields = ()
