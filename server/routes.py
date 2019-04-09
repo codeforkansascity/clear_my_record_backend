062eb78fc1b1f65f71b8427f69063d1b4b075d0d
gabby ortman <8785339+nucle0tides@users.noreply.github.com>
2019-04-08 19:07:08 -0500

    Conviction, Charge and Client models, model schemas, and GETs (#34)
    Added marshmallow for serialization of data, added charges/convictions/clients to models, added GET endpoints for these entities.

--- server/routes.py
+++ server/routes.py
@@ -1,10 +1,10 @@
-from flask import request, Response, abort
+from flask import request, Response, abort, jsonify
 from clear_my_record_backend.server import cmr, models, dbs
+from clear_my_record_backend.server.schemas import user_schema, client_schema, conviction_schema, convictions_schema, charge_schema, charges_schema
 from flask_jwt_extended import (jwt_required, create_access_token,
                                 get_jwt_identity)
 from flask_restful import Resource
 from datetime import datetime
-
 
 @cmr.route('/')
 @cmr.route('/index')
@@ -25,7 +25,7 @@ def qualifying_questions():
 @cmr.route('/qualifying_answer', methods=['POST'])
 def qualifying_answer():
     if not request.json:
-        abort(400)
+        return Response('No JSON data', status=400, mimetype='test/plain')
 
     qualifying_answer = {
         "user_session": request.json['user_session'],
@@ -41,3 +41,124 @@ def qualifying_answer():
     dbs.session.commit()
 
     return Response('Success', status=200, mimetype='text/plain')
+
+
+@cmr.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    usr = models.User.query.get(user_id)
+    if usr is None:
+        return Response("User with ID {} not found".format(user_id), status=404, mimetype='text/plain')
+    result = user_schema.dump(usr)
+
+    return jsonify(result.data)
+
+
+@cmr.route('/users/<int:user_id>', methods=['POST'])
+def update_user(user_id):
+    # return id
+    usr = models.User.query.get(user_id)
+    pass
+
+
+@cmr.route('/clients', methods=['POST'])
+def add_client():
+    # return id
+    pass
+
+
+@cmr.route('/clients/<int:client_id>', methods=['GET'])
+def get_client(client_id):
+    cli = models.Client.query.get(client_id)
+    if cli is None:
+        return Response("Client with ID {} not found".format(cli_id), status=404, mimetype='text/plain')
+    result = client_schema.dump(cli)
+
+    return jsonify(result.data)
+
+
+@cmr.route('/clients/<int:client_id>', methods=['POST'])
+def update_client(client_id):
+    #return id
+    pass
+
+
+@cmr.route('/clients/<int:client_id>', methods=['DELETE'])
+def delete_client(client_id):
+    pass
+
+
+@cmr.route('/clients/<int:client_id>/convictions', methods=['GET'])
+def get_client_convictions(client_id):
+    client_convictions = models.Conviction.query.filter(models.Client.id==client_id).all()
+    if client_convictions is None:
+        return Response("Could not find any convictions for Client {}".format(client_convictions, status=404, mimetype='text/plain'))
+    result = convictions_schema.dump(client_convictions)
+
+    return jsonify(result.data)
+
+
+
+@cmr.route('/clients/<int:client_id>/convictions', methods=['POST'])
+def add_client_conviction(client_id):
+    # return id
+    pass
+
+
+@cmr.route('/convictions/<int:conviction_id>', methods=['GET'])
+def get_conviction(conviction_id):
+    conviction = Conviction.query.get(conviction_id)
+    if conviction is None:
+        return Response("Conviction with ID {} not found".format(conviction_id), status=404, mimetype='text/plain')
+    result = conviction_schema.dump(cli)
+
+    return jsonify(result.data)
+
+
+@cmr.route('/convictions/<int:conviction_id>', methods=['POST'])
+def update_conviction(conviction_id):
+    # return id
+    pass
+
+
+@cmr.route('/convictions/<int:conviction_id>', methods=['DELETE'])
+def delete_conviction(conviction_id):
+    conviction = Conviction.query.get(conviction_id)
+    pass
+
+
+@cmr.route('/clients/<int:client_id>/convictions/<int:conviction_id>/charges', methods=['GET'])
+def get_client_charges(client_id, conviction_id):
+    charges = models.Charge.query.filter(models.Client.id==client_id and models.Conviction.id==conviction_id).all()
+    if charges is None:
+        return Response("Could not find charges for client {} and conviction {}".format(client_id, conviction_id), status=404, mimetype='text/plain')
+    result = charges_schema.dump(charges)
+
+    return jsonify(result.data)
+
+
+
+@cmr.route('/clients/<int:client_id>/convictions/<int:conviction_id>/charges', methods=['POST'])
+def add_client_charges(client_id, conviction_id):
+    pass
+
+
+@cmr.route('/charges/<int:charge_id>', methods=['GET'])
+def get_charge(charge_id):
+    charge = models.Charge.query.get(charge_id)
+    if charge is None:
+        return Response("Could not find charge with ID {}".format(charge_id), status=404, mimetype='text/plain')
+
+    result = charge_schema.dump(charge)
+
+    return jsonify(result.data)
+
+
+@cmr.route('/charges/<int:charge_id>', methods=['PUT'])
+def update_charge(charge_id):
+    pass
+
+
+@cmr.route('/charges/<int:charge_id>', methods=['DELETE'])
+def delete_charge(charge_id):
+    pass
+# don't forget to return id when doing post
---
+++ server/schemas.py
@@ -0,0 +1,88 @@
+from clear_my_record_backend.server import ma
+from clear_my_record_backend.server.models import *
+
+
+class ChargeSchema(ma.ModelSchema):
+    conviction = ma.Nested('ConvictionSchema', exclude=('charges', ))
+
+    class Meta:
+        model = Charge
+        fields = (
+            'id',
+            'conviction_id',
+            'charge',
+            'citation',
+            'sentence',
+            'class_type',
+            'charge_type',
+            'eligible',
+            'conviction'
+        )
+
+class ConvictionSchema(ma.ModelSchema):
+    charges = ma.Nested(ChargeSchema, many=True)
+    client = ma.Nested('ClientSchema', exclude=('convictions', ))
+
+    class Meta:
+        model = Conviction
+        fields = (
+            'id',
+            'client_id',
+            'client',
+            'case_number',
+            'agency',
+            'court_name',
+            'court_city_county',
+            'judge',
+            'record_name',
+            'release_status',
+            'release_date',
+            'charges',
+        )
+
+class ClientSchema(ma.Schema):
+    convictions = ma.Nested(ConvictionSchema, many=True, exclude=('client', ))
+    # exclude clients to avoid recursion issues lmao
+    user = ma.Nested('UserSchema', exclude=('clients', ))
+
+    class Meta:
+        model = Client
+        fields = (
+            'id',
+            'full_name',
+            'phone',
+            'email',
+            'sex',
+            'race',
+            'dob',
+            'address_line_1',
+            'address_line_2',
+            'city',
+            'state',
+            'zip_code',
+            'license_number',
+            'license_issuing_state',
+            'license_expiration_date',
+            'status',
+            'active',
+            'user',
+            'user_id',
+            'convictions',
+        )
+
+class UserSchema(ma.ModelSchema):
+    clients = ma.Nested(ClientSchema, many=True, exclude=('user', 'user_id', ))
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'clients')
+
+# schema instantiation for routes.py
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+client_schema = ClientSchema()
+clients_schema = ClientSchema(many=True)
+conviction_schema = ConvictionSchema()
+convictions_schema = ConvictionSchema(many=True)
+charge_schema = ChargeSchema()
+charges_schema = ChargeSchema(many=True)
