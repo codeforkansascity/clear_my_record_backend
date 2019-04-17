8eb827b3d365b95979a7ec3810dce9d7dda70a64
gabby ortman <8785339+nucle0tides@users.noreply.github.com>
2019-04-17 12:42:16 -0500

    Update routes.py

--- server/core/routes.py
+++ server/core/routes.py
@@ -211,6 +211,8 @@ def add_client_conviction(client_id):
     if request.json:
         if "release_date" in request.json:
             request.json["release_date"] = datetime.strptime(request.json["release_date"], '%Y-%m-%d').date()
+        if "arrest_date" in request.json:
+            request.json["arrest_date"] = datetime.strptime(request.json["arrest_date"], '%Y-%m-%d').date()   
         try:
             conviction.update(request.json)
             dbs.session.add(conviction)
@@ -251,7 +253,9 @@ def update_conviction(conviction_id):
         return Response("Conviction with ID {} not found".format(conviction_id), status=404, mimetype='text/plain')
 
     if "release_date" in request.json:
-        request.json["release_date"] = datetime.strptime(request.json["dob"], '%Y-%m-%d').date()
+        request.json["release_date"] = datetime.strptime(request.json["release_date"], '%Y-%m-%d').date()
+    if "arrest_date" in request.json:
+        request.json["arrest_date"] = datetime.strptime(request.json["arrest_date"], '%Y-%m-%d').date()
 
     updated_conviction = None
 
